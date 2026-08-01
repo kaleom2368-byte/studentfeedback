be614b01ee57bebc86b16c0b6a384c85411008ef
Om Kale <kaleom368@gmail.com>
2026-07-31 18:39:30 -0700

    Update MySQL connection for Aiven

--- db.js
+++ db.js
@@ -1,21 +1,40 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQLPORT
-});
 
-connection.connect((err) => {
-    if (err) {
-        console.error("❌ MySQL Connection Failed");
-        console.error(err);
-        return;
+    host: process.env.MYSQLHOST,
+
+    user: process.env.MYSQLUSER,
+
+    password: process.env.MYSQLPASSWORD,
+
+    database: process.env.MYSQL_DATABASE,
+
+    port: process.env.MYSQLPORT,
+
+    ssl: {
+        rejectUnauthorized: false
     }
 
-    console.log("✅ Connected to Railway MySQL");
 });
+
+
+connection.connect((err) => {
+
+    if (err) {
+
+        console.error("❌ MySQL Connection Failed");
+
+        console.error(err);
+
+        return;
+
+    }
+
+
+    console.log("✅ Connected to Aiven MySQL");
+
+});
+
 
 module.exports = connection;
